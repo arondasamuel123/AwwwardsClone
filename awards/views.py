250b773052bf7ead1c06d36a4d5a9a2e5ce5d563
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-14 22:14:53 +0300

    Users can register into the application

--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
+
 
 
 def home(request):
     
     return render(request, 'home.html')
 
+def signup(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            login(request,user)
+            return redirect(home)
+    
+    else:
+        form = SignUpForm()
+    return render(request,'auth/registration.html',{"form":form})
