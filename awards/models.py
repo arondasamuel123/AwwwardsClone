f89557901d5990be3b7703bca91419f0d154c9d6
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-14 14:57:16 +0300

    Save project test passed and method created

--- awards/models.py
+++ awards/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cloudinary import CloudinaryField
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -7,7 +7,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     phone_number = models.IntegerField()
-    email = models.EmailField()
     
 class Project(models.Model):
     project_title = models.CharField(max_length=20)
@@ -16,6 +15,9 @@ class Project(models.Model):
     project_link = models.URLField()
     date_posted = models.DateTimeField(auto_now_add=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+    def save_project(self):
+        self.save()
 
 class Review(models.Model):
     design= models.IntegerField()
--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Project,Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ProjectModelTestCase(TestCase):
+    '''
+    Test Class for Project Model
+    '''
+    def setUp(self):
+        self.user_one = User(username='arondasamuel123', email='arondasamuel123@gmail.com', password='123456')
+        self.profile_one = Profile(profile_photo='path/image.png',user=self.user_one, bio="I am great developer",phone_number='0791019910')
+        self.project_one = Project(project_title='GithubSearch', project_description='Project one', project_link='/path/screenshot.png',profile=self.profile_one)
+        
+        
+    def test_save_project(self):
+        self.user_one.save()
+        self.profile_one.save()
+        self.project_one.save_project()
+        
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)>0)
+        
+
